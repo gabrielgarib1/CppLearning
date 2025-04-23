ad30353d3356ad446df7d21409613b7591d3f073
gabrielgarib1 <gabrielgaribg@gmail.com>
2025-04-22 23:22:35 -0300

    organizing workspace and coding a new leet code exercise

--- .zLeetcodeChallenges/2.UnexpectedDemand/unex_demand.cpp
+++ .zLeetcodeChallenges/2.UnexpectedDemand/unex_demand.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+
 using std::vector, std::cout;
-int filledOrders(vector<int> order, int k);
+int filledOrders(vector<int> orders, int k);
+
 
 int main()
 {
+    vector<int> orders = {20,10,15,20,14,11,10};
 
-    filledOrders(vector<int> order, int k);
-
+    int k = 200;
+    int result = filledOrders(orders,k);
+    cout<<"Result: "<<result;    
 }
-int filledOrders(vector<int> order, int k) {
+
+
+
+
+int filledOrders(vector<int> orders, int k) {
 
     int max_number_of_orders=0;
-    
-    for(int i=0;i<order[0];i++)
-    {   
-        if (order[i+1]>=k)
+    int size = orders.size();
+
+    std::sort(orders.begin(),orders.end());
+
+    for(int i=0; i<size; i++)
+    {        
+        if (orders[i]<=k)
         {
             max_number_of_orders++;
-            k-=order[i+1];
+            k-=orders[i];
         }
     }
     return max_number_of_orders;
